6198e0b0eb751d18322bb3eae9bba4018c1ac377
devdanpark <sp3440@columbia.edu>
2017-10-30 20:39:18 +0900

    10.30 swift study

---
+++ convert_data_type.swift
@@ -0,0 +1,36 @@
+//Convert Data type
+
+import UIKit
+
+var strNumber:String = "1234"
+
+var intNumber:Int = Int(strNumber)
+//print(intNumber) = 1234
+
+var intNumber2 = Int(strNumber)
+
+//Convert int to String
+
+var intString = 1234
+var strString1:String?= String(intString)
+var strString2 = String(intString)
+//print(strString2) = "1234"
+
+var doubleNumber:Double = 1.34
+var intDouble = Int(doubleNumber) 
+//print(intDouble) = 1
+
+//convert double to string
+var strDouble = String(doubleNumber)
+//print(strDouble) = "1.34"
+
+//convert to double
+
+var intNumberIn = 123
+var doubleNumber1 = Double(strNumber)
+var doubleInt = Double(intNumberIn)
+
+//convert to boolean
+var strBoolean = "true"
+var boolStr = Bool(strBoolean)
+
---
+++ variable.swift
@@ -0,0 +1,31 @@
+// studying swift
+// variables
+
+import UIKit
+
+var name = "Daniel"
+print(name)
+
+var age = 25
+print(age)
+
+var department:String = "CEO"
+print(department)
+
+var average:Double = 250
+
+var isMarried:Bool = "false"
+print(isMarried)
+
+var jobTitle:String?// default nill;
+jobTitle = "CEO"
+print(jobTitle)
+//Optional("CEO")
+print(jobTitle!)
+//CEO
+
+var CreditNumber = "123456789"
+print(CreditNumber)
+
+let creditNunmber = "12345"
+// Constant Number -> cannot be changed
