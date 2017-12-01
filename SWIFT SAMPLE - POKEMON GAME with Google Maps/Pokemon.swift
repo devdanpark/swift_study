b91284b5a1657b4e941269deca7880043f7b0853
devdanpark <sp3440@columbia.edu>
2017-12-01 19:32:01 +0900

    POKEMON GAME with Google Maps
    Simple Pokemon game by using swift and google maps

---
+++ SWIFT SAMPLE - POKEMON GAME with Google Maps/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  Pokemon Game
+//
+//  Created by Seonghun Park on 2017. 11. 28..
+//  Copyright © 2017년 Seonghun Park. All rights reserved.
+//
+
+import UIKit
+
+class Pokemon: NSObject {
+
+}
