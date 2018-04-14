4802fc1a99a2fb6a8e6463de9bcd2f5cb214ce8a
devdanpark <sp3440@columbia.edu>
2018-04-15 00:38:45 +0900

    DICEE APP

---
+++ DICEE/DICEE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DICEE
+//
+//  Created by Seonghun Park on 2018. 4. 15..
+//  Copyright © 2018년 Seonghun Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
